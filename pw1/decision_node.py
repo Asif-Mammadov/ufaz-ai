a799d87831420ee25e0749c2eb778d9c1a0ddfbc
Asif <asif.mammad@hotmail.com>
2022-03-13 19:52:18 +0400

    added documentation

--- pw1/decision_node.py
+++ pw1/decision_node.py
@@ -1,8 +1,16 @@
 import copy
 import utils
 import numpy as np
+import pandas as pd
+
 class Decision_Node:
-  def __init__(self, dataset, N_groups=3, distinctionColumn='Species'):
+  def __init__(self, dataset:pd.DataFrame, N_groups:int=3, distinctionColumn:str='Species'):
+    """
+    Args:
+        dataset (pd.DataFrame): Iris dataset.
+        N_groups (int, optional): Number of groups to separate. Defaults to 3.
+        distinctionColumn (str, optional): Column separating dataset types. Defaults to 'Species'.
+    """
     self.dataset = dataset
     self.N_groups = N_groups
     self.distinctionColumn = distinctionColumn
@@ -11,7 +19,14 @@ class Decision_Node:
     self.dataset_entropy = utils.entropy(self.dataset, self.dataset[distinctionColumn].unique())
     self.attr_groups_entropy = self.find_attr_groups_entropy()
 
-  def separate_in_attr_groups(self):
+
+  def separate_in_attr_groups(self) -> dict:
+    """Separates the dataset into `self.N_groups` groups according to attribute's (column's) sorted values.
+
+    Returns:
+        dict: with a key as an dataset attribute and a value as an array of groups with length `self.N_groups`.
+        dict[attribute] = [group0, group1, ...]
+    """
     dataset = self.get_dataset()
     attributes = dataset.columns[1:-1]
     groups_by_attr = {}
@@ -20,40 +35,63 @@ class Decision_Node:
         sorted_data = dataset.sort_values(attribute).loc[:, [attribute, self.distinctionColumn]]
         groups = []
         for i in range(self.N_groups):
-            groups.append(sorted_data[int(i*N/3):int((i+1)*N/3)])
+            groups.append(sorted_data[int(i*N/self.N_groups):int((i+1)*N/self.N_groups)])
         groups_by_attr[attribute] = groups
     return groups_by_attr
 
-  def count_group(self, group_df, species=None):
-      if not species:
-        species=self.dataset[self.distinctionColumn].unique()
-      counted_df = group_df.groupby(['Species']).count()
-      for specimen in species:
-          if not counted_df.index.isin([specimen]).any():
-              counted_df.loc[specimen, :] = 0
-              counted_df = counted_df.astype(np.int64)
-      counted_df.reset_index(level=0,inplace=True)
-      return counted_df
+  def count_group(self, group_df:pd.DataFrame, species:list=None) -> pd.DataFrame:
+    """Counts a number of instances of each class in a single group.
 
-  def count_attr_groups(self):
-      attr_groups = self.get_attr_groups()
-      groups_by_species_counted = copy.deepcopy(attr_groups)
-      for attr in groups_by_species_counted.keys():
-          for group in range(len(groups_by_species_counted[attr])):
-              groups_by_species_counted[attr][group] = self.count_group(groups_by_species_counted[attr][group])
-      return groups_by_species_counted
+    Args:
+        group_df (pd.DataFrame): Group of an attribute.
+        species (list, optional): Species to consider. Defaults to None.
+
+    Returns:
+        pd.DataFrame: Counted value of each class in a group.
+    """
+    if not species:
+      species=self.dataset[self.distinctionColumn].unique()
+    counted_df = group_df.groupby(['Species']).count()
+    for specimen in species:
+        if not counted_df.index.isin([specimen]).any():
+            counted_df.loc[specimen, :] = 0
+            counted_df = counted_df.astype(np.int64)
+    counted_df.reset_index(level=0,inplace=True)
+    return counted_df
+
+  def count_attr_groups(self) -> dict:
+    """Counts a number of instances of each class for all attributes and groups.
+
+    Returns:
+        dict: of attributes and groups with counted classes.
+        dict[attribute] = [group0, group1, ...]
+    """
+    attr_groups = self.get_attr_groups()
+    groups_by_species_counted = copy.deepcopy(attr_groups)
+    for attr in groups_by_species_counted.keys():
+        for group in range(len(groups_by_species_counted[attr])):
+            groups_by_species_counted[attr][group] = self.count_group(groups_by_species_counted[attr][group])
+    return groups_by_species_counted
 
   def print_attr_groups_counted(self):
-      groups = self.get_attr_groups_counted()
-      for attr in groups.keys():
-          print("Attribute: ", attr)
-          for group in range(len(groups[attr])):
-              print("\nGroup {}:".format(group))
-              print(groups[attr][group], end='\n')
-          print("-" * 100)
+    """Prints a table of attribute and its groups with counted number of classes. 
+    """
+    groups = self.get_attr_groups_counted()
+    for attr in groups.keys():
+        print("Attribute: ", attr)
+        for group in range(len(groups[attr])):
+            print("\nGroup {}:".format(group))
+            print(groups[attr][group], end='\n')
+        print("-" * 100)
 
-  def find_attr_groups_entropy(self):
-    attr_groups_counted = self.attr_groups_counted
+  def find_attr_groups_entropy(self) -> dict:
+    """Finds the entropy of each group for all attributes and groups.
+
+    Returns:
+        dict: of attributes and groups with entropies.
+        dict[attribute] = [entropy0, entropy1, ...]
+    """
+    attr_groups_counted = self.get_attr_groups_counted()
     attr_groups_entropy = {}
     for attr in attr_groups_counted:
         groups = []
@@ -63,6 +101,8 @@ class Decision_Node:
     return attr_groups_entropy
 
   def print_attr_groups_entropy(self):
+    """Prints a table of entropies for each group of attributes. 
+    """
     attr_groups_entropy = self.attr_groups_entropy
     for attr in attr_groups_entropy:
         print(attr)
@@ -70,10 +110,16 @@ class Decision_Node:
             print("Group {} : {}".format(group, attr_groups_entropy[attr][group]))
         print("-" * 50)
 
-  def get_group_size(self, group):
-    return group.iloc[:, 1].sum()
+  def get_discriminative_power(self, columnName:str, distinctionColumn:str='Species') -> float:
+    """Gets discriminative power of a given attribute against `distinctionColumn`.
 
-  def get_discriminative_power(self, columnName, distinctionColumn='Species'):
+    Args:
+        columnName (str): Attribute(feature) to calculate discriminative power.
+        distinctionColumn (str, optional): Unique classes in a dataset. Defaults to 'Species'.
+
+    Returns:
+        float: Discriminative power of a `columnName`.
+    """
     dataset = self.dataset
     single_attr_groups_counted = {columnName: self.get_attr_groups_counted()[columnName]}
     dataset_entropy = utils.entropy(dataset, dataset[distinctionColumn].unique())
@@ -81,11 +127,14 @@ class Decision_Node:
     N = 0
     s = dataset_entropy
     for i_group in range(len(single_attr_groups_counted[columnName])):
-      N += self.get_group_size(single_attr_groups_counted[columnName][i_group])
+      N += utils.get_group_size(single_attr_groups_counted[columnName][i_group])
     for i_group in range(len(single_attr_groups_counted[columnName])):
-      s -= (self.get_group_size(single_attr_groups_counted[columnName][i_group]) / N ) * single_attr_groups_enropy[columnName][i_group]
+      s -= (utils.get_group_size(single_attr_groups_counted[columnName][i_group]) / N ) * single_attr_groups_enropy[columnName][i_group]
     return s
+
   def print_discriminative_powers(self):
+    """Prints a table of discriminative powers for all attributes. 
+    """
     for attr_name in self.get_attr_groups_counted():
         print(attr_name, ":", self.get_discriminative_power(attr_name))
 
--- pw1/utils.py
+++ pw1/utils.py
@@ -1,9 +1,27 @@
 import numpy as np
 import pandas as pd
-def sort_species(dataset, specimen):
+def sort_species(dataset:pd.DataFrame, specimen:str) -> pd.DataFrame:
+    """Filters `dataset` according to `specimen`.
+
+    Args:
+        dataset (pd.DataFrame): Iris dataset.
+        specimen (str): a unique class in a 'Species' column.
+
+    Returns:
+        pd.DataFrame: Filtered to a certain `specimen`.
+    """
     return dataset[dataset['Species'] == specimen]
 
-def entropy(dataset, instances):
+def entropy(dataset: pd.DataFrame, instances: list) -> float:
+    """Calculates the entropy of a `dataset` with `instances`.
+
+    Args:
+        dataset (pd.DataFrame): Iris dataset.
+        instances (list): unique classes in a dataset.
+
+    Returns:
+        float: Entropy value.
+    """
     s = 0
     N = dataset.shape[0]
     for specimen in instances:
@@ -12,8 +30,30 @@ def entropy(dataset, instances):
     s *= -1
     return s
 
-def entropy_group(group_counted_df, distinctionColumn='Species', N=50):
+def get_group_size(group:pd.DataFrame) -> int:
+    """Gets number of instances in a group.
+
+    Args:
+        group (pd.DataFrame): Consists of instances of classes. 
+
+    Returns:
+        int: Number of total instances in a group.
+    """
+    return group.iloc[:, 1].sum()
+
+def entropy_group(group_counted_df: pd.DataFrame, distinctionColumn:str='Species') -> float:
+    """Calculates the group entropy.
+
+    Args:
+        group_counted_df (pd.DataFrame): Group with counted instances according to a class (`distinctionColumn`)
+        distinctionColumn (str, optional): Column to differentiate between classes in a dataset. Defaults to 'Species'.
+        N (int, optional): Number of instances. Defaults to 50.
+
+    Returns:
+        float: _description_
+    """
     s = 0
+    N = get_group_size(group_counted_df)
     for specimen in group_counted_df[distinctionColumn]:
         n_specimen = group_counted_df.set_index(distinctionColumn).loc[specimen, :][0]
         if n_specimen != 0:
