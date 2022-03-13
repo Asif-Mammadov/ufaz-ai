64efbd9b19ed35707e432bfe0eb3190c6ac471f5
Asif <asif.mammad@hotmail.com>
2022-03-13 20:08:26 +0400

    minor fix

--- pw1/main.py
+++ pw1/main.py
@@ -1,28 +1,28 @@
-from pw1.decision_node import Decision_Tree
+from decision_node import Decision_Node
 import pandas as pd
 
 dataset = pd.read_csv("Iris.csv", delimiter=',')
-dt = Decision_Tree(dataset)
+dt = Decision_Node(dataset)
+dt.print_attr_groups_counted()
+
+# dt.print_attr_groups_entropy()
+
+# for attr_name in dt.get_attr_groups_counted():
+#   print(attr_name, ":", dt.get_discriminative_power(attr_name))
 
 
-dt.print_attr_groups_entropy()
+# new_dataset = dataset[dataset['PetalLengthCm'] > dataset.sort_values('PetalLengthCm')['PetalLengthCm'].reset_index(drop=True)[50]].reset_index(drop=True)
+# print(new_dataset.sort_values('PetalLengthCm'))
+# # print(new_dataset)
+# dt2 = Decision_Tree(new_dataset, N_groups=2)
 
-for attr_name in dt.get_attr_groups_counted():
-  print(attr_name, ":", dt.get_discriminative_power(attr_name))
+# for attr_name in dt2.get_attr_groups_counted():
+#   print(attr_name, ":", dt2.get_discriminative_power(attr_name))
 
+# # # dt2.print_attr_groups_entropy()
+# dt2.print_attr_groups_counted()
 
-new_dataset = dataset[dataset['PetalLengthCm'] > dataset.sort_values('PetalLengthCm')['PetalLengthCm'].reset_index(drop=True)[50]].reset_index(drop=True)
-print(new_dataset.sort_values('PetalLengthCm'))
-# print(new_dataset)
-dt2 = Decision_Tree(new_dataset, N_groups=2)
-
-for attr_name in dt2.get_attr_groups_counted():
-  print(attr_name, ":", dt2.get_discriminative_power(attr_name))
-
-# # dt2.print_attr_groups_entropy()
-dt2.print_attr_groups_counted()
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.pairplot(new_dataset.iloc[:, 1:], hue='Species', size=2)
-plt.show()
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# sns.pairplot(new_dataset.iloc[:, 1:], hue='Species', size=2)
+# plt.show()
