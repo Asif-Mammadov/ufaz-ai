c2e5d9be7ae1a15663dd3a6043030addfab26cc2
asif-mammadov <asif.mammad@hotmail.com>
2022-03-22 22:59:32 +0400

    optimal changes

--- pw4/test.py
+++ pw4/test.py
@@ -1,6 +1,6 @@
 import numpy as np
 from neuralNetwork import NeuralNetwork
-from activation import Relu
+from activation import Relu, Sigmoid
 import utils
 
 
@@ -8,15 +8,16 @@ def main():
     X = np.array([[0, 1, 0, 1], [0, 0, 1, 1]])
     Y = np.array([[0, 1, 1, 0]])
 
-    X = np.tile(X, 600)
-    Y = np.tile(Y, 600)
+    X = np.tile(X, 120)
+    Y = np.tile(Y, 120)
     X_train, Y_train, X_test, Y_test = utils.split_train_test(X, Y)
 
     # return
-    nn = NeuralNetwork(X, Y, lr=0.1, batch_size=50)
-    # nn.add_hidden_layer(5, Relu)
+    nn = NeuralNetwork(X_train, Y_train, lr=0.1, batch_size=None)
+    nn.add_hidden_layer(5, Sigmoid)
     nn.info()
-    nn.training_epoch(n_epochs=5, verbose=False)
+    nn.training_epoch(n_epochs=500, verbose=True)
+
     nn.testPrediction(X_test, Y_test)
     nn.plot_costs()
 
